4d9952632a60526aaf351c1bfdb288b09216b84f
ry2294 <ry2294@columbia.edu>
2015-11-26 22:59:13 -0500

    Worker App

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser      = require('body-parser');
+
+var app = express();
+var http = require('http').Server(app);
+
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
+app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+
+var router = express.Router();
+
+router.post('/tweet', function(req, res) {
+	console.log('req body = ' + req.body);
+	res.status(200).end();
+});
+
+app.use('/', router);
+
+var server = http.listen(process.env.PORT || 8888, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
