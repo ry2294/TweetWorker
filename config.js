1876f59f39c0c0ea7f690c89b8c107aff5482174
ry2294 <ry2294@columbia.edu>
2015-11-28 01:30:09 -0500

    config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+config.dynamodb = {};
+config.sqs = {};
+
+config.dynamodb.accessKeyId = '';
+config.dynamodb.secretAccessKey = '';
+config.dynamodb.region = '';
+config.sqs.queueurl = '';
+
+module.exports = config;
