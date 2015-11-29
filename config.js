6fb433541d2adc2107cf11f6dee83d61653e4e30
ry2294 <ry2294@columbia.edu>
2015-11-28 20:41:41 -0500

    Pushing everything

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ var config = {};
 config.dynamodb = {};
 config.sqs = {};
 
-config.dynamodb.accessKeyId = '';
-config.dynamodb.secretAccessKey = '';
-config.dynamodb.region = '';
-config.sqs.queueurl = '';
+config.dynamodb.accessKeyId = 'AKIAJLRX34F3XGDNR62Q';
+config.dynamodb.secretAccessKey = '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';
+config.dynamodb.region = 'us-west-2';
+config.sqs.queueurl = 'https://sqs.us-west-2.amazonaws.com/545137376042/tweet';
 
 module.exports = config;
