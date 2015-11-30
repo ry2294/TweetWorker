5af1deefe59e4c1b309841c4f51a7bc5cba5bdbe
ry2294 <ry2294@columbia.edu>
2015-11-29 22:26:03 -0500

    Fetching Trends

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ var config = {};
 
 config.dynamodb = {};
 config.sqs = {};
+config.twitter = {};
 
+config.twitter.consumerKey = 'sjXP9IKOPpq4lzvLtGvSy6a3R';
+config.twitter.consumerSecret = 'UeHHDyAke0ZympTeNU6ysZOERPeXvbQbGIaU0EPDN8lVNVkJP7';
+config.twitter.accessTokenKey = '3949975300-keFgncsWq5qkDgl8Dj5Q2HhB07Qw72S19LnF3dn';
+config.twitter.accessTokenSecret = 'tbP35dyp5m2mLp1VPCrdOALPhivTxxFA7ZhTtPShgOhjh';
 config.dynamodb.accessKeyId = 'AKIAIVIRGE7VAIAVZK6A';
 config.dynamodb.secretAccessKey = '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';
 config.dynamodb.region = 'us-west-2';
--- dynamodb.js
+++ dynamodb.js
@@ -1,11 +1,20 @@
 var AWS = require('aws-sdk');
 var config = require('./config');
 var unirest = require('unirest');
+var Twitter = require('twitter');
+var _ = require('underscore');
 
 AWS.config.update({
     accessKeyId: config.dynamodb.accessKeyId, 
     secretAccessKey: config.dynamodb.secretAccessKey,
     region: config.dynamodb.region});
+
+var client = new Twitter({
+    consumer_key: config.twitter.consumerKey,
+    consumer_secret: config.twitter.consumerSecret,
+    access_token_key: config.twitter.accessTokenKey,
+    access_token_secret: config.twitter.accessTokenSecret
+});
 
 var dynamodb= {};
 
@@ -16,17 +25,91 @@ var sns = new AWS.SNS({
     secretAccessKey: config.dynamodb.secretAccessKey,
     region: config.dynamodb.region});
 
-var putTweet = function (Item) {
+var extractTrends = function (trends) {
+    var content = "";
+    if(trends != null) {
+        if(trends.trends != null) {
+            _.each(trends.trends, function(trend) {
+                if(trend != null && trend.name != null)
+                    content += trend.name + ', ';
+            });
+        }
+    }
+    return content;
+};
+
+var getTrendsForWoeid = function(woeid, place) {
+	console.log('place = ' + JSON.stringify(place));
+	if(place.fetchTrends == 0) {
+	    client.get('trends/place', {'id':woeid}, function (error, trends, response) {
+	    	var content = "Top Trending: ";
+	        if(error) {
+	        	console.log('trends error = ' + JSON.stringify(error));
+	        	content += JSON.stringify(error);
+	        	place.fetchTrends = 0;
+	        	place.trends = content;
+	    	}
+	        else if(trends != null && trends[0] != null) {
+	            content += extractTrends(trends[0]);
+	            place.fetchTrends = 1;
+	        	place.trends = content;
+	        }
+	        putPlace(place);
+	    });
+	}
+	else {
+		putPlace(place);
+	}
+};
+
+var getPlace = function (tweet) {
+	docClient.get({
+		Key: {'woeid': tweet.woeid},
+		TableName: 'place_tbl'
+	}, function(error, data){
+    	if (error) console.log(error);
+    	else {
+    		var place = data.Item;
+    		if(place != null) {
+    			var tweetText = 'User: ' + tweet.userName 
+    					+ '<br>Sentiment: ' 
+						+ tweet.sentiment 
+						+ '<br>Tweet: ' + tweet.text;
+				var tweets = (place.tweets != null) ? place.tweets : "";
+				tweets += '<br><br>' + tweetText;
+				place.tweets = tweets;
+				place.tweetCount = (place.tweetCount != null) ? (parseFloat(place.tweetCount) + 1) : 1;
+
+				var positive = (place.positive != null) ? parseFloat(place.positive) : 0;
+				place.positive = (tweet.sentiment == 'positive') ? (positive + 1) : positive;
+
+				var negative = (place.negative != null) ? parseFloat(place.negative) : 0;
+				place.negative = (tweet.sentiment == 'negative') ? (negative + 1) : negative;
+
+				var neutral = (place.neutral != null) ? parseFloat(place.neutral) : 0;
+				place.neutral = (tweet.sentiment == 'neutral') ? (neutral + 1) : neutral;
+
+				var sentimenterror = (place.sentimenterror != null) ? parseFloat(place.sentimenterror) : 0;
+				place.sentimenterror = (tweet.sentiment == 'daily-transaction-limit-exceeded') ? (sentimenterror + 1) : sentimenterror;
+				place.x = tweet.x;
+				place.y = tweet.y;
+				getTrendsForWoeid(tweet.woeid, place);
+    		}
+    	} 
+	});
+};
+
+var putPlace = function (place) {
 	docClient.put({
-		TableName: "tweet_tbl",
-		Item: Item
+		TableName: "place_tbl",
+		Item: place
 	}, function(error, data) {
-		if (error) console.log('Error while storing sentiment');
+		if (error) console.log('Error while storing place');
 		else { 
 			console.log('Sentiment stored = ' + data);
 			sns.publish({
 				TargetArn: 'arn:aws:sns:us-west-2:545137376042:tweet',
-				Message: Item.tweetId
+				Message: place.woeid
 			}, function (error, data) {
 				if (error) console.log('sns push failed. error = ' + error);
 				else console.log('sns push succeeded. data = ' + JSON.stringify(data));
@@ -47,7 +130,7 @@ var deleteTweet = function (tweetId) {
 	});
 };
 
-var getSetiment = function (text, Item) {
+var getSetiment = function (text, tweet) {
 	var request = unirest.post('http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment');
 	request.header('Content-Type', 'application/x-www-form-urlencoded')
 	.send('apikey=2a525c85bb4e0def466e9c7dce402e7fbc43ee49')
@@ -60,14 +143,13 @@ var getSetiment = function (text, Item) {
   			response.body != null && 
   			response.body.docSentiment != null && 
   			response.body.docSentiment.type != null) {
-  			Item.sentiment = response.body.docSentiment.type;
-  			putTweet(Item);
+  			tweet.sentiment = response.body.docSentiment.type;
   		} else {
   			console.log('Not able to compute sentiment');
-  			Item.sentiment = response.body.statusInfo;
-  			putTweet(Item);
-  			//deleteTweet(Item.tweetId);
+  			tweet.sentiment = response.body.statusInfo;
   		}
+  		getPlace(tweet);
+  		deleteTweet(tweet.tweetId);
 	});
 };
 
