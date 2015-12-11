f5aeeb06fb44e5a03089fdb8a15740b879917b25
ry2294 <ry2294@columbia.edu>
2015-12-11 11:00:30 -0500

    Dumping everything

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ config.twitter.consumerKey = 'sjXP9IKOPpq4lzvLtGvSy6a3R';
 config.twitter.consumerSecret = 'UeHHDyAke0ZympTeNU6ysZOERPeXvbQbGIaU0EPDN8lVNVkJP7';
 config.twitter.accessTokenKey = '3949975300-keFgncsWq5qkDgl8Dj5Q2HhB07Qw72S19LnF3dn';
 config.twitter.accessTokenSecret = 'tbP35dyp5m2mLp1VPCrdOALPhivTxxFA7ZhTtPShgOhjh';
-config.dynamodb.accessKeyId = 'AKIAIVIRGE7VAIAVZK6A';
-config.dynamodb.secretAccessKey = '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';
+config.dynamodb.accessKeyId = 'AKIAJSRJTPIENWFUVFYA';
+config.dynamodb.secretAccessKey = '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';
 config.dynamodb.region = 'us-west-2';
 config.sqs.queueurl = 'https://sqs.us-west-2.amazonaws.com/545137376042/tweet';
 
